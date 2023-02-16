cf119ad73152b8a589360c707ebea2fa4c9619ec
LelyH <lely2011@gmail.com>
2023-02-15 22:54:37 -0800

    updated readMe file

---
+++ client/src/utils/queries.js
@@ -0,0 +1,17 @@
+import gql from "graphql-tag";
+
+export const GET_ME = gql`
+    {me{
+        _id
+        username
+        email
+        savedBooks {
+            title
+            bookId
+            authors
+            description
+            image
+            link
+        }
+    }}
+`;
